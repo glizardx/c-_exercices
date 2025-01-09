43a05e155838f8bea93bf1c9bde076c07bb9d004
Guilherme Lizardo <guilherme.lizardo@amf.edu.br>
2025-01-08 22:08:22 -0300

    v2

--- cSharp_exercices/exercice1.cs
+++ cSharp_exercices/exercice1.cs
@@ -1,34 +1,37 @@
-﻿static void Main()
+﻿class Program
 {
-    string question1 = "What is the capital of Brazil?";
-    string answer1 = "Brasilia";
-
-    string question2 = "What is the capital of France?";
-    string answer2 = "Paris";
-
-    int score = 0;
-
-    Console.WriteLine(question1);
-    string userAnswer1 = Console.ReadLine();
-    if (userAnswer1.Trim().ToLower() == answer1.ToLower())
+    static void Main()
     {
-        Console.WriteLine("Correct");
-        score = score + 1;
-    }
-    else
-    {
-        Console.WriteLine("Incorrect, the answer is " + answer1);
-    }
-    
-    Console.WriteLine("Quiz complete. Your score is " + score + "/2");
-    if (score == 3)
-    {
-        Console.WriteLine("Congratulations, you got a perfect score!");
-    }
-    else
-    {
-        Console.WriteLine("Better luck next time!");
-    }
+        string question1 = "What is the capital of Brazil?";
+        string answer1 = "Brasilia";
 
-    Console.ReadKey();
+        string question2 = "What is the capital of France?";
+        string answer2 = "Paris";
+
+        int score = 0;
+
+        Console.WriteLine(question1);
+        string userAnswer1 = Console.ReadLine();
+        if (userAnswer1.Trim().ToLower() == answer1.ToLower())
+        {
+            Console.WriteLine("Correct");
+            score = score + 1;
+        }
+        else
+        {
+            Console.WriteLine("Incorrect, the answer is " + answer1);
+        }
+        
+        Console.WriteLine("Quiz complete. Your score is " + score + "/2");
+        if (score == 3)
+        {
+            Console.WriteLine("Congratulations, you got a perfect score!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+
+        Console.ReadKey();
+    }
 }
