e02057c1f3eb90997ebc383a4faaf2dc89c5644c
Guilherme Lizardo <guilherme.lizardo@amf.edu.br>
2025-01-08 22:30:07 -0300

    ex

--- cSharp_exercices/exercice2.cs
+++ cSharp_exercices/exercice2.cs
@@ -4,9 +4,8 @@ Console.WriteLine($"Num is {num}");
 
 // incrementing 
 num++;
-Console.WriteLine($"Num is {num}"); //string interpolation
-Console.Write("Num is {0}", ++num); // string formatting
-Console.Write("Num is ", num); // string concatenation
-
+Console.WriteLine($"Num is {num}"); // string interpolation
+Console.WriteLine("Num is {0}", ++num); // string formatting
+Console.WriteLine("Num is " + num); // string concatenation
 
 Console.ReadLine();
